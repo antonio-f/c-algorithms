ef93c917d181afb4b8ddd8cca0621890228d993b
Antonio <anf@tuta.io>
2024-06-26 18:39:53 +0200

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -8,7 +8,6 @@ void insertion_sort(int *arr, int n)
         {
             int key = arr[i];
             int j = i -1;
-
             while(j >= 0 && arr[j] > key)
             {
                 arr[j+1] = arr[j];
